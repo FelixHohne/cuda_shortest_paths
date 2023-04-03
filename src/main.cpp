1c2db6504b92268563c871848fb20f31d9b09cd9
Felix Hohne <felix.m.hohne@gmail.com>
2023-04-03 17:11:08 -0400

    Make CA road network eligible for Dijkstra

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,8 @@
 #include <vector>
 
 int main(int argc, char *argv[]) {
+    bool DEBUG_PRINT = false;
+
     if (argc <= 1) {
         std::cout << "Requires argument for matrix location" << std::endl;
         return 1;
@@ -13,14 +15,20 @@ int main(int argc, char *argv[]) {
 
     std::list<std::pair<int, int>> parsed_edge_list = read_edge_list(file_location);
 
-    print_edge_list(parsed_edge_list);
+    if (DEBUG_PRINT)
+        print_edge_list(parsed_edge_list);
     std::unordered_map<int, std::list<int>> adjList = constructAdjList(parsed_edge_list);
-    printAdjList(adjList);
+
+    if (DEBUG_PRINT)
+        printAdjList(adjList);
 
     auto [min_distances, p] = st_dijkstra(adjList, 0);
 
+    std :: cout << "Reporting minimum distances: " << std::endl;
+    int counter = 0;
     for (auto const distance: min_distances) {
-        std::cout << "min distance: " << distance << std :: endl;
+        std::cout << "min distance for " << counter << ": " << distance << std :: endl;
+        counter++;
     }
 
     return 0;
