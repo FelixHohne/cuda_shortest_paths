4019b453d82361343f68f7239c370ba8ba8f27ec
Shiyuan Huang <shiyuancn@live.cn>
2023-04-17 18:25:10 -0700

    Remove unneeded code from all.sh

--- all.sh
+++ all.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-CORRECTNESS="../../hw2/correctness"
-CORRECT="../../hw2/correct"
 
 if [ "$1" == "build" ]; then
   if [ -d "build" ]; then
@@ -18,7 +16,7 @@ elif [ "$1" == "run-gpu" ]; then
   make
   sbatch job-gpu
 elif [ "$1" == "status" ]; then
-  squeue -u sh2223
+  squeue -u $USER
 elif [ "$1" == "view" ]; then
   if [ -f build/slurm*.out ]; then
     cat build/slurm*.out
@@ -27,36 +25,18 @@ elif [ "$1" == "view" ]; then
   fi
 elif [ "$1" == "salloc" ]; then
   salloc --nodes 1 --qos interactive --time 01:00:00 --constraint gpu --gpus 1 --account=m4341
-elif [ "$1" == "mac" ]; then
-  if [ -d "build" ]; then
-    rm -r build
-  fi
-  mkdir build
-  cd build
-  cmake -DCMAKE_BUILD_TYPE=Release .. -DCMAKE_C_COgpuLER=/opt/homebrew/Cellar/gcc@11/11.3.0/bin/gcc-11 -DCMAKE_CXX_COgpuLER=/opt/homebrew/Cellar/gcc@11/11.3.0/bin/g++-11
 elif [ "$1" == "check-correct" ]; then
-  CORRECTNESS="../hw2/correctness"
-  CORRECT="../hw2/correct"
-  scp sh2223@perlmutter-p1.nersc.gov:~/cs5220-sp23/hw4/build/gpu.parts.out .
-  # scp sh2223@perlmutter-p1.nersc.gov:~/cs5220-sp23/hw4/build/gpu-s-42-n-100000.parts.out .
-  # scp sh2223@perlmutter-p1.nersc.gov:~/cs5220-sp23/hw4/build/gpu-s-91-n-100000.parts.out .
-  python3 $CORRECTNESS/correctness-check.py gpu.parts.out $CORRECTNESS/verf.out
-  # python3 $CORRECTNESS/correctness-check.py gpu-s-42-n-100000.parts.out $CORRECT/correct-s-42-n-100000.parts.out
-  # python3 $CORRECTNESS/correctness-check.py gpu-s-91-n-100000.parts.out $CORRECT/correct-s-91-n-100000.parts.out
+  echo "Correctness check has not yet been implemented."
 elif [ "$1" == "correct" ]; then
-  cd build
-  make correct
-  ./correct -s 42 -n 100000 -o $CORRECT/correct-s-42-n-100000.parts.out
-  ./correct -s 91 -n 100000 -o $CORRECT/correct-s-91-n-100000.parts.out
-elif [ "$1" == "tar" ]; then
-  cd build
-  rm CS5220Group60_hw4.tar.gz
-  rm -rf temp-tar
-  cmake -DGROUP_NO=60 ..
-  make package
-  tar tfz CS5220Group60_hw4.tar.gz
-  mkdir temp-tar
-  cp CS5220Group60_hw4.tar.gz temp-tar
-  cd temp-tar
-  tar -xvf CS5220Group60_hw4.tar.gz
+  # this is intended to generated the correct output
+  # haven't figured out how to run this on Perlmutter yet since I'm not sure
+  # if we're allowed to install conda, so run locally for now
+  HOSTNAME=$(uname -n)
+  if [[ "$HOSTNAME" == *"login"* ]]; then
+    echo "correct not supported on Perlmutter, please run on local machine."
+    # module load python
+  else
+    python3 src/GraphAlgorithmsPython.py /datasets/roadNet-CA.txt
+    echo "Writing outputs to python_shortest_path_outs.txt"
+  fi
 fi
