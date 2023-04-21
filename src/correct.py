7ad79b5464d4336eefcd06f16987fcd1e48e59fa
Felix Hohne <fmh42@cornell.edu>
2023-04-20 22:53:49 -0400

    Fix merge

--- src/correct.py
+++ src/correct.py
@@ -25,15 +25,16 @@ def main(argv):
       print("Number of edges for node 9721: ", G.degree[9721]) 
       print("9721 neighbors: ", G.edges(9721))
    assert G.number_of_edges() > 0 
-   p = nx.shortest_path_length(G, source=0)
+   d = nx.shortest_path_length(G, source=0)
+   d_sorted = dict(sorted(d.items()))
+   p = nx.shortest_path(G, source=0)
 
-   p_sorted = dict(sorted(p.items()))
-   
    f = open("python_shortest_path_outs.txt", "w")
    f.write("source\tdistance\n")
-   for (key, value) in p_sorted.items():
+   for (key, value) in d_sorted.items():
       f.write(f"{key}\t{value}\n")
 
+   print(p[1969993][-1]) 
  
 if __name__ == "__main__":
    main(sys.argv[1:])
